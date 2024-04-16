8476ea0655443e4e90b8d11493cc23d523980c1a
guoxi <2983166962@qq.com>
2024-04-16 15:13:19 +0800

    更新 Win_Qt.py

--- Win_Qt.py
+++ Win_Qt.py
@@ -5,13 +5,28 @@ from config import save_flag
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("参数设置")
+        self.setWindowTitle("Image Caption")
         self.setGeometry(100, 100, 300, 200)
         
         self.button = QPushButton('保存模型', self)
         self.button.setGeometry(50, 50, 200, 50)
-        self.button.clicked.connect(self.toggle_save_flag)
-        
+       
+
+        self.input_path = QLineEdit(self)
+        self.input_path.setGeometry(50, 50, 300, 30)
+        self.predict_button = QPushButton('生成文本', self)
+        self.predict_button.setGeometry(50, 100, 100, 30)
+    
+        self.result_label = QLabel(self)
+        self.result_label.setGeometry(50, 150, 300, 300)
+        self.result_label.setScaledContents(True)  # 让图片适应label的大小
+self.predict_button.clicked.connect(self.predict_image)
+         self.button.clicked.connect(self.toggle_save_flag)
+    def predict_image(self):
+        image_path = self.input_path.text()
+        processed_image = pre(image_path)
+        pixmap = QPixmap(processed_image)
+        self.result_label.setPixmap(pixmap)
     def toggle_save_flag(self):
         global save_flag
         save_flag = not save_flag
