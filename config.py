6f4fa48aea14e76e26ccb3691e28d081b5102d6c
xiaoxi <2983166962@qq.com>
2024-04-23 14:07:08 +0800

    只是一个定时程序你可以忽略它，因为并没有优化的很好

--- config.py
+++ config.py
@@ -13,8 +13,6 @@ class Config:
         # 记录上次保存的时间
         self.last_save_time = time.time()
 
-        # 检查是否超过超时时间，如果是，则将保存参数设置为True
-
     def check_timeout(self):
         current_time = time.time()
         if current_time - self.last_save_time > self.timeout:
